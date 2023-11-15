903f28c9bf4b57d123cff099e05eca0520a29501
Xuesong Huang <z5441891@ad.unsw.edu.au>
2023-11-15 11:25:32 +1100

    Update LoginForm.js

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,1 +1,48 @@
+// LoginForm.js
+import React, { useEffect, useState } from 'react';
+import authService from './authService';
+import { useAuth } from './AuthContext';
+import { useNavigate } from 'react-router';
 
+const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { isLoggedIn, login } = useAuth();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate('/');
+    }
+  }, [isLoggedIn, navigate]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await authService.login(email, password);
+      login(response.data.token);
+    } catch (error) {
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+        required
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+        required
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default LoginForm;
