2d8bc2a3d594366a198eae079905a1874e49b2a0
Xuesong Huang <z5441891@ad.unsw.edu.au>
2023-11-16 22:58:44 +1100

    Update AllListing.js

--- frontend/src/Listing/AllListing.js
+++ frontend/src/Listing/AllListing.js
@@ -1,142 +1,84 @@
-import React, { useState } from 'react';
-import { TextField, Button, Box, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
-import listingService from './listingService';
-import { useNavigate } from 'react-router';
+import React, { useEffect, useState } from 'react';
+import Listing from './Class/listing';
+import Review from './Class/review';
+import {
+  Box,
+  Paper,
+  Rating,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow
+} from '@mui/material';
 
-function CreateListing () {
-  const navigate = useNavigate();
-  const [formData, setFormData] = useState({
-    title: '',
-    price: '',
-    thumbnail: '',
-    propertyType: '',
-    numberOfBeds: '',
-    numberOfBathrooms: '',
-    street: '',
-    city: '',
-    state: '',
-    zipCode: ''
+function AllListing () {
+  const [listings, setListings] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
 
-  });
+        const response = await listingService.getAllListing();
+        const listingsData = response.data.listings; 
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
+        return await Promise.all(listingsData.map(async (item) => {
+          const detailResponse = await listingService.getListingDetail(item.id);
+          const data = detailResponse.data.listing;
+          const metadata = data.metadata;
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const fullData = {
-      title: formData.title,
-      price: formData.price,
-      thumbnail: formData.thumbnail,
-      address: {
-        street: formData.street,
-        city: formData.city,
-        state: formData.state,
-        zipCode: formData.zipCode
-      },
-      metadata: {
-        propertyType: formData.propertyType,
-        numberOfBeds: formData.numberOfBeds,
-        numberOfBathrooms: formData.numberOfBathrooms,
-        reviews: []
+          const reviews = metadata.reviews.map(review => new Review(review.reviewer, review.rating, review.comment));
+
+          return new Listing(item.id, data.title, metadata.propertyType, metadata.numberOfBeds, metadata.numberOfBathrooms, data.thumbnail, reviews, data.price);
+        }));
+      } catch (error) {
+        console.error('Error fetching data:', error);
       }
     };
-    try {
-      const response = await listingService.createListing(fullData);
-      alert('Listing created:' + response.data.listingId);
-      navigate('/hosted-listings')
-    } catch (error) {
-      console.error('Error creating listing:', error);
-    }
-  };
 
+    fetchData().then(setListings);
+  }, []);
   return (
-    <Box component="form" onSubmit={handleSubmit} sx={{ '& .MuiTextField-root': { m: 1 }, maxWidth: 500, mx: 'auto' }}>
-      <TextField
-        name="title"
-        label="Listing Title"
-        value={formData.title}
-        onChange={handleChange}
-        fullWidth
-      />
-      <TextField
-        name="price"
-        label="Price (per night)"
-        value={formData.price}
-        onChange={handleChange}
-        fullWidth
-        type="number"
-      />
-      <TextField
-        name="thumbnail"
-        label="Thumbnail URL"
-        value={formData.thumbnail}
-        onChange={handleChange}
-        fullWidth
-      />
-      <FormControl fullWidth sx={{ m: 1 }}>
-        <InputLabel>Property Type</InputLabel>
-        <Select
-          name="propertyType"
-          value={formData.propertyType}
-          label="Property Type"
-          onChange={handleChange}
-        >
-          <MenuItem value="Villa">Villa</MenuItem>
-          <MenuItem value="Apartment">Apartment</MenuItem>
-          {/* 可以添加更多的物业类型 */}
-        </Select>
-      </FormControl>
-      <TextField
-        name="street"
-        label="Street Address"
-        value={formData.street}
-        onChange={handleChange}
-        fullWidth
-      />
-      <TextField
-        name="city"
-        label="City"
-        value={formData.city}
-        onChange={handleChange}
-        fullWidth
-      />
-      <TextField
-        name="state"
-        label="State"
-        value={formData.state}
-        onChange={handleChange}
-        fullWidth
-      />
-      <TextField
-        name="zipCode"
-        label="Zip Code"
-        value={formData.zipCode}
-        onChange={handleChange}
-        fullWidth
-      />
-      <TextField
-        name="numberOfBeds"
-        label="Number of Beds"
-        value={formData.numberOfBeds}
-        onChange={handleChange}
-        fullWidth
-        type="number"
-      />
-      <TextField
-        name="numberOfBathrooms"
-        label="Number of Bathrooms"
-        value={formData.numberOfBathrooms}
-        onChange={handleChange}
-        fullWidth
-        type="number"
-      />
-      {}
-      <Button type="submit" variant="contained" sx={{ mt: 2 }}>Create Listing</Button>
+    <Box m={3} p={1}>
+      <TableContainer component={Paper}>
+        <Table aria-label="listings table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Title</TableCell>
+              <TableCell align="right">Property Type</TableCell>
+              <TableCell align="right">Beds</TableCell>
+              <TableCell align="right">Bathrooms</TableCell>
+              <TableCell align="right">Thumbnail</TableCell>
+              <TableCell align="right">Rating</TableCell>
+              <TableCell align="right">Reviews</TableCell>
+              <TableCell align="right">Price (per night)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {listings.map((listing) => (
+              <TableRow key={listing.id}>
+                <TableCell component="th" scope="row">
+                  {listing.title}
+                </TableCell>
+                <TableCell align="right">{listing.propertyType}</TableCell>
+                <TableCell align="right">{listing.numberOfBeds}</TableCell>
+                <TableCell align="right">{listing.numberOfBathrooms}</TableCell>
+                <TableCell align="right">
+                  <img src={listing.thumbnail} alt="Thumbnail" style={{ width: '50px' }} />
+                </TableCell>
+                <TableCell align="right">
+                  <Rating value={listing.averageRating} precision={0.1} readOnly />
+                </TableCell>
+                <TableCell align="right">{listing.reviews.length}</TableCell>
+                <TableCell align="right">${listing.pricePerNight}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Box>
+
   );
 }
 
-export default CreateListing;
+export default AllListing;
