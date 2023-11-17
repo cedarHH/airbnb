4f59f0bd05610d09746e91f2efcac2d093541730
Xuesong Huang <z5441891@ad.unsw.edu.au>
2023-11-17 18:33:31 +1100

    Update LoginForm.js

--- frontend/src/components/Auth/LoginForm.js
+++ frontend/src/components/Auth/LoginForm.js
@@ -1,13 +1,14 @@
-// LoginForm.js
 import React, { useEffect, useState } from 'react';
 import { useAuth } from './AuthContext';
 import { useNavigate } from 'react-router';
+import { TextField, Button, Container, Box, Paper, Typography } from '@mui/material';
 
 const LoginForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const { isLoggedIn, login } = useAuth();
   const navigate = useNavigate();
+
   useEffect(() => {
     if (isLoggedIn) {
       navigate('/'); 
@@ -18,28 +19,56 @@ const LoginForm = () => {
     event.preventDefault();
     try {
       login(email, password);
+
     } catch (error) {
+
+      alert('Login error: ');
+      console.log(error.response)
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Email"
-        required
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Password"
-        required
-      />
-      <button type="submit">Login</button>
-    </form>
+    <Container component="main" maxWidth="xs">
+      <Paper elevation={6} sx={{ marginTop: 8, padding: 4, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Login
+          </Button>
+        </Box>
+      </Paper>
+    </Container>
   );
 };
 
