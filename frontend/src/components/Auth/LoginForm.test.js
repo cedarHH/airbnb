e8dfc99c9a9980b1a572009d4b3213527280bdbe
Xuesong Huang <z5441891@ad.unsw.edu.au>
2023-11-17 18:48:03 +1100

    Add new file

---
+++ frontend/src/components/Auth/LoginForm.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import LoginForm from './LoginForm';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './AuthContext';
+
+
+const renderLoginForm = (isLoggedIn = false) => {
+  const mockLogin = jest.fn();
+  return render(
+    <BrowserRouter>
+      <AuthProvider value={{ isLoggedIn, login: mockLogin }}>
+        <LoginForm />
+      </AuthProvider>
+    </BrowserRouter>
+  );
+};
+
+describe('LoginForm Component', () => {
+  test('renders email and password input fields and a login button', () => {
+    renderLoginForm();
+    expect(screen.getByLabelText(/email address/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/password/i)).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /login/i })).toBeInTheDocument();
+  });
+
+  test('allows entering email and password', () => {
+    renderLoginForm();
+    fireEvent.change(screen.getByLabelText(/email address/i), { target: { value: 'test@example.com' } });
+    fireEvent.change(screen.getByLabelText(/password/i), { target: { value: 'password123' } });
+    expect(screen.getByLabelText(/email address/i).value).toBe('test@example.com');
+    expect(screen.getByLabelText(/password/i).value).toBe('password123');
+  });
+});
