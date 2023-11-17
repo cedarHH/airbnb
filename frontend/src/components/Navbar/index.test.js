f70ad868cb449808bc1071d600a84424b6be8f6d
Boran Liu <z5432784@ad.unsw.edu.au>
2023-11-17 18:58:54 +1100

    Update file index.test.js

---
+++ frontend/src/components/Navbar/index.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './index';
+import { AuthProvider } from '../Auth/AuthContext';
+
+const renderNavbar = (isLoggedIn) => {
+  const logoutMock = jest.fn();
+  return render(
+    <BrowserRouter>
+      <AuthProvider value={{ isLoggedIn, logout: logoutMock }}>
+        <Navbar />
+      </AuthProvider>
+    </BrowserRouter>
+  );
+};
+
+describe('Navbar Component', () => {
+  test('renders login and register buttons when not logged in', () => {
+    renderNavbar(false);
+    expect(screen.getByText('Login')).toBeInTheDocument();
+    expect(screen.getByText('Register')).toBeInTheDocument();
+    expect(screen.queryByText('Logout')).not.toBeInTheDocument();
+  });
+});
