72a656da6abea798182eb3c559c5121ff3521e1a
Xuesong Huang <z5441891@ad.unsw.edu.au>
2023-11-17 21:24:46 +1100

    Update address.js

--- frontend/src/components/Listing/Class/address.js
+++ frontend/src/components/Listing/Class/address.js
@@ -1,11 +1,11 @@
 class Address {
-    constructor (street, city, state, zipCode, unit = '') {
-      this.street = street;
-      this.city = city;
-      this.state = state;
-      this.zipCode = zipCode;
-      this.unit = unit;
-    }
+  constructor (street, city, state, zipCode, unit = '') {
+    this.street = street;
+    this.city = city;
+    this.state = state;
+    this.zipCode = zipCode;
+    this.unit = unit;
   }
-  
-  export default Address;
+}
+
+export default Address;
