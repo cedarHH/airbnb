4269acf6b3482f72bff5e2012eb01688ba34d980
Xuesong Huang <z5441891@ad.unsw.edu.au>
2023-11-17 18:34:41 +1100

    Update RegisterForm.js

--- frontend/src/components/Auth/RegisterForm.js
+++ frontend/src/components/Auth/RegisterForm.js
@@ -1,7 +1,7 @@
-// RegisterForm.js
 import React, { useState } from 'react';
-import authService from './authService'; 
+import authService from './authService';
 import { useNavigate } from 'react-router';
+import { TextField, Button, Container, Box, Paper, Typography } from '@mui/material';
 
 const RegisterForm = () => {
   const [email, setEmail] = useState('');
@@ -13,36 +13,65 @@ const RegisterForm = () => {
     event.preventDefault();
     try {
       await authService.register(email, password, name);
-      navigate('/login'); 
-    } catch (error) {   
+      navigate('/login');
+    } catch (error) {
+      alert('Register error: ' + error.response.data.error);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Email"
-        required
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Password"
-        required
-      />
-      <input
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        placeholder="Name"
-        required
-      />
-      <button type="submit">Register</button>
-    </form>
+    <Container component="main" maxWidth="xs">
+      <Paper elevation={6} sx={{ marginTop: 8, padding: 4, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <Typography component="h1" variant="h5">
+          Register
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="name"
+            label="Name"
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Register
+          </Button>
+        </Box>
+      </Paper>
+    </Container>
   );
 };
 
