5f78e1d370aa38a49a1ed7881b833870051eac98
Xuesong Huang <z5441891@ad.unsw.edu.au>
2023-11-17 21:28:51 +1100

    Update index.test.js

--- frontend/src/components/Navbar/index.test.js
+++ frontend/src/components/Navbar/index.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from './index';
 import { AuthProvider } from '../Auth/AuthContext';
